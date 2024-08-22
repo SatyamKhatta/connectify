921549c6bdf75b876249ed543c980478482f2b32
SatyamKhatta <149493210+SatyamKhatta@users.noreply.github.com>
2024-08-22 22:55:12 +0530

    design a lobby using css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
-export default function Home() {
- 
+import { useRouter } from "next/router"
+import { v4 as uuidv4 } from "uuid"
+import styles from '@/styles/Home.module.css'
+import { useState } from "react";
 
+export default function Home() {
+  
+  const router =  useRouter();
+  const [roomId,setRoomId] =useState('')
+  const createAndJoin = ()=> {
+    const roomId = uuidv4();
+    router.push(`/${roomId}`)
+  }
+  const joinRoom = () =>{
+  if(roomId){
+    router.push(`/${roomId}`)
+  }else{
+    alert("provide a valid Room id ")
+  }
+  }
     return (
-    <h1>welcome</h1>
+   <div className={styles.homeContainer}>
+    <h1>Connectify </h1>
+    <div className={styles.enterRoom}>
+      <input placeholder="enter Room Id" value={roomId} onChange={(e)=> setRoomId(e?.target?.value)}/>
+      <button onClick={joinRoom}>Join Room</button>
+    </div>
+    <span className={styles.sepratorText}>----------OR-----------</span>
+    <button onClick={createAndJoin}>
+      Create new room
+    </button>
+   </div>
   )
 }
